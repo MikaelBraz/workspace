a1b1e229b75f4a5bb6b6a9fbfa5f2c9eb290ea4b
MikaelBraz <mikaelpbraz@gmail.com>
2023-10-17 21:49:01 -0300

    AulaJs02

---
+++ JS/Js/Aula02-Variaveis/main.js
@@ -0,0 +1,61 @@
+var nomeDoPrimeiroAluno = "Guilherme";
+
+var nomeDoPrimeiroAluno = "Guilherme";
+
+console.log(nomeDoPrimeiroAluno)
+
+// Let
+
+let numeroDeAprovados = 5;
+
+numeroDeAprovados = 6;
+
+console.log(numeroDeAprovados);
+
+console.clear()
+
+// const
+const nomeSiteWeb = 6;
+
+console.log(nomeSiteWeb);
+
+console.clear ();
+
+var numero1 = 2;
+
+var numero2 = 4;
+
+var soma = numero1 + numero2;
+
+console.log(soma)
+
+// String
+
+var texto = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit sem ex, id ullamcorper magna pretium eget. Donec vitae dolor lobortis, volutpat justo quis, tincidunt eros. Vestibulum nec lorem in neque tincidunt pellentesque a dignissim eros. Nulla purus nibh, pellentesque vitae tincidunt sed, gravida vitae augue. Integer id augue venenatis, feugiat sapien ac, finibus risus. Proin lacinia felis quis luctus gravida. Integer lorem dui, tempor nec hendrerit non, maximus sed quam. Mauris faucibus arcu et sodales porta. Vestibulum at egestas tortor. Pellentesque eget interdum metus. Vestibulum pellentesque ornare mauris eget ultricies. In dui ligula, tristique et fermentum et, commodo accumsan magna. Vestibulum at mi rhoncus sapien tincidunt fringilla.";
+
+console.log (texto);
+
+console.clear ()
+
+var texto1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris hendrerit sem ex, id ullamcorper magna pretium eget. Donec vitae dolor lobortis, volutpat justo quis, tincidunt eros. Vestibulum nec lorem in neque tincidunt pellentesque a dignissim eros. Nulla purus nibh, pellentesque vitae tincidunt sed, gravida vitae augue. Integer id augue venenatis, feugiat sapien ac, finibus risus. Proin lacinia felis quis luctus gravida. Integer lorem dui, tempor nec hendrerit non";
+
+console.log(texto1)
+
+var texto2 = "aximus sed quam. Mauris faucibus arcu et sodales porta. Vestibulum at egestas tortor. Pellentesque eget interdum metus. Vestibulum pellentesque ornare mauris eget ultricies. In dui ligula, tristique et fermentum et, commodo accumsan magna. Vestibulum at mi rhoncus sapien tincidunt fringilla."
+
+console.log(texto2)
+
+var concatenar = texto1 + "/n" + texto2;
+
+console.log(concatenar)
+
+console.clear()
+
+// Booleanos
+
+var osDeVerdadeEuSeiQuemE = true;
+
+console.clear
+
+console.log ("five" * 2git)
+
