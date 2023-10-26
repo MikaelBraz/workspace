2942f12bb5518631607ab9e7d8b65fe85cefccb9
MikaelBraz <mikaelpbraz@gmail.com>
2023-10-25 21:41:30 -0300

    Aula05js

--- JS/Js/Aula05_Repetição/main.js
+++ JS/Js/Aula05_Repetição/main.js
@@ -12,3 +12,19 @@ let contador = 1
 
 //do while
 
+contador = 1;
+
+
+do {
+    console.log(`O número do contador é 
+    ${contador}`);
+    contador++; // increment the contador variable
+} while(contador <= 10);
+
+console.clear()
+
+for(let contador = 1; contador <= 10; contador++)
+{
+    console.log(`O numero do contador é
+    ${contador}`)
+}
