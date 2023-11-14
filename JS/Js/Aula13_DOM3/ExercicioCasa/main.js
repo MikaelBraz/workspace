1ac3e3fdc505416dab88c6f7fe22c32ee96612c3
Mikael Braz <143458686+MikaelBraz@users.noreply.github.com>
2023-11-13 22:43:24 -0300

    Atualizar o main.js

--- JS/Js/Aula13_DOM3/ExercicioCasa/main.js
+++ JS/Js/Aula13_DOM3/ExercicioCasa/main.js
@@ -1,12 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const botao1 = document.getElementById("B_01");
-    botao1.onclick = function() {
-      const imagem = document.createElement("img");
-      imagem.src = "./img/UmbrellaAcademy.jpg"; // Substitua com a URL da imagem desejada
-  
-      document.body.style.backgroundColor = "#262525"; // Mude a cor de fundo do body
-  
-      const body = document.body;
-      body.appendChild(imagem); // Insira a imagem ao lado do botão
-    };
-  });
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora de Dívida</title>
+    <script src="script.js" defer></script>
+</head>
+<body>
+
+    <label for="principal">Valor Principal: </label>
+    <input type="number" id="principal" placeholder="Insira o valor principal">
+
+    <label for="taxa">Taxa de Juros (%): </label>
+    <input type="number" id="taxa" placeholder="Insira a taxa de juros">
+
+    <label for="parcelas">Número de Parcelas: </label>
+    <input type="number" id="parcelas" placeholder="Insira o número de parcelas">
+
+    <button onclick="calcularDivida()">Calcular Dívida</button>
+
+    <p id="resultado"></p>
+
+</body>
+</html>
