585e2fdb015c158bd0b8bada613dfc544889ef79
Mikael Braz <143458686+MikaelBraz@users.noreply.github.com>
2023-11-18 17:05:14 -0300

    Atualizar o Principal.jsx

---
+++ React/primeiro-site-web/src/components/Principal.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react';
+
+const Principal = () => {
+  const filmes = ['Filme 1', 'Filme 2', 'Filme 3', 'Filme 4', 'Filme 5'];
+
+  return (
+    <main style={{ backgroundColor: 'orange', color: 'purple', padding: '20px' }}>
+      <p style={{ fontSize: '2rem' }}>
+        Estou participando do curso de IOS com o objetivo de adquirir conhecimentos fundamentais na área de desenvolvimento front-end. Meu intuito é aprender a colaborar eficientemente em projetos de equipe, aprimorar minhas habilidades de comunicação e, a longo prazo, evoluir como desenvolvedor.
+      </p>
+      <ul>
+        {filmes.map((filme, index) => (
+          <li key={index}>{filme}</li>
+        ))}
+      </ul>
+      <img src="caminho_para_a_imagem_do_filme_1.jpg" alt="Filme 1" />
+    </main>
+  );
+};
+
+export default Principal;
